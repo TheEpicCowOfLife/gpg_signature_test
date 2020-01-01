0b66e3102c20a88d1a91e567494abbdec35b21eb
TheEpicCowOfLife <38208392+TheEpicCowOfLife@users.noreply.github.com>
2020-01-02 01:50:00 +1100

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,8 @@ print("""
 Just checking if my GPG signature works through this commit.
 I'm not entirely sure how this reinforces my authenticity, 
 since it looks like it's still gated behind a regular password.
+
+Attempt 2: It looks like github already does some sort of default gpg
+signature by default, so it'd be awesome if someone could explain to me
+why this practice isn't totally obsoleted by this feature.
 """)
