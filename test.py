57986d0eaa6e2b772b5fdaab795395fa12ad43b5
TheEpicCowOfLife <38208392+TheEpicCowOfLife@users.noreply.github.com>
2020-01-02 01:40:33 +1100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,5 @@
+print("""
+Just checking if my GPG signature works through this commit.
+I'm not entirely sure how this reinforces my authenticity, 
+since it looks like it's still gated behind a regular password.
+""")
