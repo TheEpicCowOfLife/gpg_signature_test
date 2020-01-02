6561e4e15649bf19af9fcc4512f404efaa264981
TheEpicCowOfLife <TheEpicCowOfLife@gmail.com>
2020-01-02 19:18:29 +1100

    Update test.py

--- test.py
+++ test.py
@@ -6,4 +6,7 @@ since it looks like it's still gated behind a regular password.
 Attempt 2: It looks like github already does some sort of default gpg
 signature by default, so it'd be awesome if someone could explain to me
 why this practice isn't totally obsoleted by this feature.
+
+Attempt 3: Looks like I have to do this using git... I guess it's time for me to learn
+how to use that then.
 """)
